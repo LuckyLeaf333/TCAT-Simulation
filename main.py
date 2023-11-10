8fe677b59cdcaffefb2b36af4185a73e8728af63
ivanzhe <ivanzheng445@gmail.com>
2023-11-10 00:26:04 -0500

    updated code

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from PassengerClass import Passenger
 from BusClass import Bus
+from RouteClass import Route
 import csv
 
 with open('Passenger.csv', newline='') as csvfile:
@@ -22,15 +23,15 @@ with open('Bus.csv', newline='') as csvfile:
         
         bus_list.append(Bus(attribute[0], attribute[1], attribute[2], attribute[3]))
 
-with open('r.csv', newline='') as csvfile:
-    reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    next(reader)
-    bus_list = []
-    for row in reader:
-        attribute = []
-        [attribute.append(int(i)) for i in row[0].split(",")]
+# with open('r.csv', newline='') as csvfile:
+#     reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+#     next(reader)
+#     bus_list = []
+#     for row in reader:
+#         attribute = []
+#         [attribute.append(int(i)) for i in row[0].split(",")]
         
-        bus_list.append(Bus(attribute[0], attribute[1], attribute[2], attribute[3]))
+#         bus_list.append(Bus(attribute[0], attribute[1], attribute[2], attribute[3]))
 
 
 
