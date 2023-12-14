d84e6e6ad7a40fa59bf030a7291cfa016102a8a4
JiaJia Ye <52677649+LuckyLeaf333@users.noreply.github.com>
2023-12-13 19:29:29 -0500

    Added comments to simpyBus.py

--- simpyBus.py
+++ simpyBus.py
@@ -4,18 +4,26 @@ import networkx as nx
 
 
 class Passenger:
+
+    """
+    id: Unique id number of the passenger
+    start: The bus stop the passenger begins at
+    target: The bus stop the passenger wants to go to
+    """
     def __init__(self, id, start, target):
         self.id = id
         self.target = target
         self.start = start
 
+
 class Bus(object):
+
     """
-    env: Simpy environment
-    id: Id number for the bus (integer)
-    stopList: list of the names of stops, the nodes the bus stops at
-    travelTimes: travelTimes[a] is the time needed to travel from stopList[a] to stopList[a + 1].
-                    If the stop is the last stop in the list, it is the time from stopList[a] to stopList[0]
+    env: SimPy environment simulation runs in
+    id: unique id number of the bus
+    capacity: Maximum number of passengers the bus can have
+    stopList: A list of the stops along the bus' route. Assumes the route is a cycle
+    graph: A graph of the stops in the bus system. The stopList must contain a valid cycle in this graph.
     """
     def __init__(self, env, id, capacity, stopList, graph):
         self.env = env
@@ -31,11 +39,19 @@ class Bus(object):
         # Start the run process everytime an instance is created.
         self.action = env.process(self.run())
 
+    """
+    Get the index of the next stop in stopList. Loops back to the beginning of the stopList when the end is reached.
+    Assumes the route is a loop.
+    """
     def nextStopIndex(self):
         if self.stopIndex == len(self.stopList) - 1:
             return 0
         return self.stopIndex + 1
 
+    """
+    Make the bus travel along its route. Pick up and drop off passengers if appropriate.
+    Report when the bus leaves a certain stop.
+    """
     def run(self):
         while True:
             # Travel to next stop
@@ -50,12 +66,21 @@ class Bus(object):
             yield self.env.process(self.dropOffPassengers(5))
             print("Bus %d is leaving stop %s at time %d" % (self.id, self.stopName, self.env.now))
 
+    """
+    Move the bus to its next stop. Report when the bus arrives at its next stop.
+    duration: The amount of time it takes to travel to the next stop.
+    """
     def goToNextStop(self, duration):
         yield self.env.timeout(duration)
         self.stopIndex = self.nextStopIndex()
         self.stopName = self.stopList[self.stopIndex]
         print("Bus %d has arrived at stop %s at time %d" % (self.id, self.stopName, self.env.now))
 
+    """
+    Pick up passengers at the current stop if there are passengers to pick up. 
+    Reports which passengers are picked up, if any.
+    duration: The amount of time it takes to board passengers 
+    """
     def pickUpPassengers(self, duration):
 
         stopPassengerList = self.graph.nodes[self.stopName]['passengers']
@@ -88,14 +113,23 @@ class Bus(object):
         yield self.env.timeout(duration)
         print("Bus %d has boarded passengers %sat time %d" % (self.id, outputString, self.env.now))
 
+    """
+    Drop off passengers, if the current stop is the passengers' destination.
+    Report which passengers were dropped off, if any.
+    duration: The amount of time it takes to drop off passengers.
+    """
     def dropOffPassengers(self, duration):
-        dropOff = False
-        dropOffList = []
+        dropOff = False  # True if any passengers should be dropped off
+        dropOffList = []  # List of passengers to drop off
+
         for passenger in self.passengerList:
+            # Determine if any passengers should be dropped off
             if passenger.target == self.stopName:
                 dropOff = True
                 dropOffList.append(passenger)
+
         if dropOff:
+            # Drop off passengers in dropOffList
             yield self.env.timeout(duration)
             outputString = ""
             for passenger in dropOffList:
@@ -111,26 +145,32 @@ times = [5, 10, 7, 9, 8, 5, 6, 11, 6]
 graph = nx.Graph()
 graph.add_edges_from(edgeList)
 
+# Set the nodes in the graph to have lists of passengers waiting at those nodes
 passengerList = {1: [Passenger(1, 1, 5), Passenger(2, 1, 1)], 2: [], 3: [], 4:[], 5:[Passenger(3, 5, 8), Passenger(4, 5, 1)],
                  6:[], 7:[], 8:[]}
 nx.set_node_attributes(graph, passengerList, 'passengers')
 
+# Set the time needed to travel from one node to the next
 i = 0
 for (u, v) in edgeList:
     graph[u][v]['time'] = times[i]
     i += 1
 
+# Visualize the graph
 nx.draw_networkx(graph, positions, node_size=500, node_color='darkorange')  # draw graph
 nx.draw_networkx_edge_labels(graph, positions,
                                      edge_labels=nx.get_edge_attributes(graph, 'times'))
 plt.show()
 
+# Create the simulation environment
 env = simpy.Environment()
 
+# Create the buses and routes
 stopList1 = (1, 2, 3, 4, 5, 6)
 bus1 = Bus(env, 1, 40, stopList1, graph)
 
 stopList2 = (5, 7, 8)
 bus2 = Bus(env, 2, 50, stopList2, graph)
 
+# Run the simulation for a certain number of time steps
 env.run(until=100)
